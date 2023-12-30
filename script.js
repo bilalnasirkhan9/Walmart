3cd7b2c45dfd04f62003a9a1ebb6721d1f6ae72c
Bilal Nasir <bilal2308f@aptechorangi.com>
2023-12-30 02:29:12 -1200

    Created a Toggle Menu
    Toggle Menu for Mobile User to gave them good experience

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ function getaddressystem() {
     }
     isDropdownVisible = !isDropdownVisible; // Toggle the state
 }
+
+const mainMoveLeft = document.getElementById('layout-container')
+let menuToggle = document.getElementById('menu-toggle')
+let menuToggleClose = document.getElementById('menu-toggle-close')
+let menuToggleCont = document.getElementById('toggle-menu-container')
+let toggleItems = document.getElementById('toggle-items')
+
+
+
+function toggleclick(){
+    if (!isDropdownVisible) {;
+            menuToggleCont.style.width = '200px'
+            mainMoveLeft.style.marginLeft = '200px' // Display the dropdown menu
+            toggleItems.style.display = 'block'
+            toggleItems.style.opacity = '1'
+    } else {
+        menuToggleCont.style.width = '0px'
+        mainMoveLeft.style.marginLeft = '0px' // Hide the dropdown menu
+        toggleItems.style.display = 'none'
+        toggleItems.style.opacity = '0'
+    }
+    isDropdownVisible = !isDropdownVisible; // Toggle the state
+}
+
+// function helo() {
+//     if(isDropdownVisible){
+//         menuToggleCont.style.width = '0px'
+//         mainMoveLeft.style.marginLeft = '0px'
+//     }else{
+        
+
+//     }
+
+    
+// }
