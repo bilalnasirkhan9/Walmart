6b3f39cd21ef2673cffc1de5b06bd205ad814c02
Bilal Nasir <bilal2308f@aptechorangi.com>
2024-01-01 17:57:05 -1200

    Create a Toggle List
    Toggle List create for given the more excellent experience to users

--- script.js
+++ script.js
@@ -18,18 +18,20 @@ let menuToggleClose = document.getElementById('menu-toggle-close')
 let menuToggleCont = document.getElementById('toggle-menu-container')
 let toggleItems = document.getElementById('toggle-items')
 
-
-
 function toggleclick(){
     if (!isDropdownVisible) {
-            menuToggleCont.style.width = '100vw'
+            menuToggleCont.style.width = '320px'
             toggleItems.style.display = 'block'
             toggleItems.style.opacity = '1'
+            document.body.style.position = 'fixed';
+            document.body.style.width = '100%';
         
     } else {
-        menuToggleCont.style.width = '0vw'
+        menuToggleCont.style.width = '0px'
         toggleItems.style.display = 'none'
         toggleItems.style.opacity = '0'
+        document.body.style.position = '';
+        document.body.style.width = '';
 
     }
     isDropdownVisible = !isDropdownVisible; // Toggle the state
